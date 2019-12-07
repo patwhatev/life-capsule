439ee853cd4987cf2a76e04fece7dd7b34486e82
patwhatev <patrickeverman@Patricks-MacBook-Pro.local>
2019-12-07 09:05:36 -0500

    Updates

---
+++ precache-manifest.4e001ef9212144446e364c3725ea7db1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be05b442a66f6ecb6463943069ca681e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e35c20cfc095c1bd3d3",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "18248e081731d5ee1915",
+    "url": "/static/css/main.7b8fbfaa.chunk.css"
+  },
+  {
+    "revision": "5e35c20cfc095c1bd3d3",
+    "url": "/static/js/2.3d4aa551.chunk.js"
+  },
+  {
+    "revision": "18248e081731d5ee1915",
+    "url": "/static/js/main.98f9ff98.chunk.js"
+  },
+  {
+    "revision": "13acef260b67a98d4bfe",
+    "url": "/static/js/runtime-main.9c1c1653.js"
+  }
+]);
