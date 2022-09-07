cdbf9108760dbe988e24c72f5ed7ef7afffbb755
Patrick Everman <patrick.everman@bouncex.com>
2022-09-07 19:12:48 -0400

    Updates

--- precache-manifest.b55c89776702a3163504826f0b29bc93.js
+++ precache-manifest.b55c89776702a3163504826f0b29bc93.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e9e210ef3e442f979398b0ba60b984e0",
+    "revision": "636794bed795402167b98d45bb7e54f7",
     "url": "/index.html"
   },
   {
-    "revision": "cc9c7c3fc63b89aa56af",
+    "revision": "71dabbe02f534a20b592",
     "url": "/static/css/2.0624914a.chunk.css"
   },
   {
-    "revision": "22ef84ecb3e0ff0ef462",
+    "revision": "16ac9d817512b3a01be3",
     "url": "/static/css/main.072c57ba.chunk.css"
   },
   {
-    "revision": "cc9c7c3fc63b89aa56af",
-    "url": "/static/js/2.5fd59129.chunk.js"
+    "revision": "71dabbe02f534a20b592",
+    "url": "/static/js/2.bca0baee.chunk.js"
   },
   {
-    "revision": "22ef84ecb3e0ff0ef462",
-    "url": "/static/js/main.45b7d777.chunk.js"
+    "revision": "16ac9d817512b3a01be3",
+    "url": "/static/js/main.34f2d279.chunk.js"
   },
   {
     "revision": "13acef260b67a98d4bfe",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6678406969c88d9a74f05720d1a1271a.js"
+  "/precache-manifest.b55c89776702a3163504826f0b29bc93.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
